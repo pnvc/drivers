bb0cffc49e840a42f3745bba41c57d18d7c4590f
wbj0t <theonetwothe@gmail.com>
2024-06-06 17:09:42 +0400

    .

--- 9chapter-communic_with_hw/io.c
+++ 9chapter-communic_with_hw/io.c
@@ -8,17 +8,24 @@
 /* chapter 9 */
 #include <linux/ioport.h>
 #include <asm/io.h>
-#include <asm-generic/io.h>
 
 #define IO "io"
 #define IOM "io: "
 #define RET_SUCCESS 0
 #define RET_FAILURE -1
 #define NR_DEVICES 1
-#define IO_START 0x378
-#define IO_NR 0x8
+#define IO_START 0xa0000
+#define IO_NR 0x100000 - IO_START
+#define X "%X\n"
+#define D "%d\n"
+#define C "%c\n"
+#define P "%p\n"
+#define S "%s\n"
 
-static int major = 0, minor = 0;
+static int major = 0, minor = 0, use_mem = 0;
+
+/* chapter 9 */
+module_param(use_mem, int, S_IRUGO);
 
 struct io {
 	char cmd;
@@ -29,6 +36,7 @@ struct io {
 
 	/* chapter 9 */
 	struct resource *resource;
+	void *iomem;
 };
 
 static int io_open(struct inode *, struct file *);
@@ -46,6 +54,7 @@ static struct io io = {
 	}
 };
 
+/* chapter 9 */
 static int io_request_region(void)
 {
 	io.resource = request_region(IO_START, IO_NR, IO);
@@ -53,6 +62,22 @@ static int io_request_region(void)
 		pr_info(IOM"failed io_request_region\n");
 		return RET_FAILURE;
 	}
+
+	return RET_SUCCESS;
+}
+
+/* chapter 9 */
+static int io_request_mem_region(void)
+{
+	/*
+	io.resource = request_mem_region(IO_START, IO_NR, IO);
+	if (!io.resource) {
+		pr_info(IOM"failed io_request_mem_region\n");
+		return RET_FAILURE;
+	}
+	*/
+	io.iomem = ioremap(IO_START, IO_NR);
+	pr_info(IOM P, io.iomem);
 
 	return RET_SUCCESS;
 }
@@ -74,15 +99,31 @@ static ssize_t io_write(struct file *filp, const char __user *buf,
 
 	switch(iop->cmd) {
 	case '0':
-		unsigned short a = 0;
-		pr_info(IOM"cmd [ZERO]\n");
-		for (char i = 0; i < IO_NR; i++)
-			outb_p(0x0, IO_START+i);
-		wmb();
-		a = inb_p(IO_START+0);
-		rmb();
-		pr_info(IOM"%x\n", a);
+		/* chapter 9 */
+		if (use_mem) {
+			int x;
+			char str[10];
+			void __iomem *add = io.iomem;
+			iowrite16(0x3235, add);
+			//memcpy_fromio(str, add, 2);
+			pr_info(IOM S, str);
+		} else {
+			unsigned short a = 0;
+			pr_info(IOM"cmd [ZERO]\n");
+			outb_p(0x66, IO_START+0);
+			wmb();
+			a = inb_p(IO_START+0);
+			rmb();
+			pr_info(IOM"%x\n", a);
+		}
 		break;
+	case '9':
+		if (use_mem) {
+			void __iomem *add = io.iomem;
+			char str[10];
+			memcpy_fromio(str, add, 2);
+			pr_info(IOM S, str);
+		}
 	default:
 		pr_info(IOM"cmd [%c]\n", iop->cmd);
 		break;
@@ -95,7 +136,15 @@ static ssize_t io_read(struct file *filp, char __user *buf,
 			size_t count, loff_t *f_pos)
 {
 	ssize_t retval = 0;
-	return retval;
+	char kbuf[10];
+	void __iomem *add = io.iomem;
+
+	memcpy_fromio(kbuf, add, 2);
+	pr_info(IOM S, kbuf);
+
+	copy_to_user(buf, kbuf, 2);
+
+	return count;
 }
 
 
@@ -157,13 +206,18 @@ static int reg_dev(void)
 
 static int io_init(void)
 {
+	int err = 0;
 	if (
 		reg_dev()		|| /* get major */
 		io_create_class()	|| /* create class */
 		reg_cdev()		|| /* reg char device */
-		io_device_create()	|| /* create /dev/io */
-		io_request_region()	   /* get io port address range */
+		io_device_create()	   /* create /dev/io */
 	   )
+		return RET_FAILURE;
+
+			/* io mem region */	          /* io region */
+	use_mem ? (err = io_request_mem_region()) : (err = io_request_region());
+	if (err)
 		return RET_FAILURE;
 
 	pr_info(IOM"started. MAJOR [%d], MINOR [%d]\n", major, minor);
@@ -176,7 +230,11 @@ static void io_exit(void)
 	device_destroy(io.class, io.dev);
 	class_destroy(io.class);
 	unregister_chrdev_region(io.dev, NR_DEVICES);
-	release_region(IO_START, IO_NR);
+	use_mem ?
+		iounmap(io.iomem)
+//		release_mem_region(IO_START, IO_NR)
+		:
+		release_region(IO_START, IO_NR);
 	pr_info(IOM"closed\n");
 }
 
